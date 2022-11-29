b126f1319aeda9c26c3dda0e213abecbaa16f716
Vitor Magno <vmcg@ic.ufal.br>
2022-11-29 10:47:41 -0300

    construindo uma classe pra lidar com as datas

---
+++ Data/DataHandler.java
@@ -0,0 +1,51 @@
+package Data;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DataHandler {
+    private GregorianCalendar gc;
+    private Date paraManipular;
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy hh");
+
+    public DataHandler(String data){
+        try {
+            this.gc = new GregorianCalendar();
+            this.gc.setTime(formatter.parse(data));
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Date getDate(){
+        paraManipular = gc.getTime();
+        return paraManipular;
+    }
+
+    /**
+     * checa se a data de inicio eh anterior a data final
+     * @param inicio
+     * @param fim
+     * @return
+     */
+    public boolean checkDate(Date inicio, Date fim){
+        this.gc.setTime(inicio);
+        return this.gc.before(fim);
+    }
+
+    /**
+     * checa se o periodo ultrapassa a data final
+     * @param inicio
+     * @param fim
+     * @param periodo
+     * @return
+     */
+    public boolean checkPeriod(Date inicio, Date fim, int periodo){
+        this.gc.setTime(inicio);
+        this.gc.add(0, periodo);
+        return this.gc.after(fim);
+    }
+}
